cc3db07ad7c4eb073902e57f0797f1ce5a0cd4d3
nicole <njl55@cornell.edu>
2024-04-28 01:07:04 -0400

    changed inventory from json to mutable array

--- lib/inventory.ml
+++ lib/inventory.ml
@@ -1,10 +1,21 @@
-(** Inventory is the module that has all the inventory functionalities  *)
+(** Inventory is the module that has all the inventory functionalities *)
 
 open Utils
 
-type item = {
-  name : string;
-  quantity : int;
-  condition : string;
+type inventory_item = {
+  mutable empty : bool;
+  mutable item : string;
 }
+
+(** [size] is the inventory size *)
+let size = 3
+
+(** [create_inventory] is an empty inventory with 3 slots*)
+let create_inventory () =
+  Array.init size (fun _ -> {empty = true; item = "none"})
+
+(* todo: account for when empty = false... can't add *)
+let add_item inventory slot new_item =
+  inventory.(slot).item <- new_item
+
 
