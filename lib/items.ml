2ebb364541b295cfcb326ea20125922eef3f2944
Analeah Real <142811266+analeah@users.noreply.github.com>
2024-05-04 17:57:10 -0400

    Inventory + item implementations
    - Made item descriptions in new json items.json
    - Edited room1.json to now have an inventory tutorial
    - Changed inventory UI, not final
    - Made items.ml with items
    - Edits to rpg.ml for inventory tutorial and pattern matching

---
+++ lib/items.ml
@@ -0,0 +1,29 @@
+(* stores all the qualities of the items *)
+
+open Utils
+open Inventory
+let item_doc = Utils.load_json "data/items.json"
+
+let gold_egg : inventory_item = {
+  mutable health_dmg_max : 0;
+  mutable empty : false;
+  mutable item : "Gold Egg";
+}
+
+let chicken_breast : inventory_item = {
+  mutable health_dmg_max : 15;
+  mutable empty : false;
+  mutable item : "Chicken Breast";
+}
+
+let print_item item =
+  print_endline item.item;
+  if item.health_dmg_max > 0 then
+  print_string "+"^(string_of_int item.health_dmg_max)
+  else if item.health_dmg_max > 0 then
+    print_string "-"^(string_of_int item.health_dmg_max)
+  else
+    print_string "?"
+  Utils.print_msg item.item item_doc;
+  
+  
--- lib/rpg.ml
+++ lib/rpg.ml
@@ -1,5 +1,6 @@
 open Constants
 open Inventory
+open Items
 
 (* NOTE: DELETE THE DUNE FILE IN THE ROOT AFTER WE ARE DONE TESTING *)
 
@@ -39,18 +40,45 @@ let rec chicken_option () =
   in
   part ()
 
-let rec inventory_tutorial () =
+let rec inventory_option inventory =
+  Utils.print_nested_msg "inventory_tutorial" "i" room1;
   let rec part () =
     let input = read_line () in
     match input with
-    | "i" -> Inventory.print_inventory inventory
+    | "i 1" ->
+        (* first item is health bar *)
+        Inventory.print_health inventory;
+        Utils.print_msg "Health Bar" Items.item_doc
+    | "i 3" -> print_item (Inventory.item_slot_name inventory 3)
+    | "i 4" -> print_item (Inventory.item_slot_name inventory 4)
+    | "i 5" -> print_item (Inventory.item_slot_name inventory 5)
     | "h" ->
         Inventory.print_health inventory;
-        print_endline "That's not how you do it silly! It's okay, try again.\n";
+        print_endline "Cool, but not right now. Why don't you try again?\n";
         part ()
     | _ ->
         Utils.clear_screen ();
-        print_endline "That's not how you do it silly! It's okay, try again.\n"
+        print_endline "That's not how you do it silly! It's okay, try again.\n";
+        part ()
+  in
+  part ()
+
+let rec inventory_tutorial () =
+  Utils.print_nested_msg "inventory_tutorial" "prompt" room1;
+  let rec part () =
+    let input = read_line () in
+    match input with
+    | "i" ->
+        Inventory.print_inventory inventory;
+        inventory_option inventory
+    | "h" ->
+        Inventory.print_health inventory;
+        print_endline "Cool, but not right now. Why don't you try again?\n";
+        part ()
+    | _ ->
+        Utils.clear_screen ();
+        print_endline "That's not how you do it silly! It's okay, try again.\n";
+        part ()
   in
   part ()
 
