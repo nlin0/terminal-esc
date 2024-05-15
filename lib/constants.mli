363408c0529285a8ee881bbfce6a34c7fff0c387
Analeah Real <142811266+analeah@users.noreply.github.com>
2024-05-15 19:52:53 -0400

    syntax

--- lib/constants.mli
+++ lib/constants.mli
@@ -16,9 +16,6 @@ val happy_chicken : unit -> unit
 val dead_chicken : unit -> unit
 (** [dead_chicken ()] prints a dead chicken text art *)
 
-val smile_whale : unit -> unit
-(** [smile_whale ()] prints a smile whale text art *)
-
 val chest : unit -> unit
 val bug : unit -> unit
 val robo_bun : unit -> unit
